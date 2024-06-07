61ef6462fff049fb0804e6c1476fc32654c703c9
siyam79 <siyamahmed3827@gmail.com>
2024-06-07 10:03:53 +0600

    dark and light mode implements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/model-icon.png" />
       </head>
       <body className={inter.className}>
         <NextUIProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+import CustomFilter from "@/Components/CustomFilter/CustomFilter";
 import Hero from "@/Components/Hero/Hero";
+import SearchBar from "@/Components/SearchBar/SearchBar";
 
 
 export default function Home() {
   return (
     <>
       <div className="overflow-hidden">
-       <Hero />
-       
+        <Hero />
+
+        <div className='mt-12 padding-x padding-y max-width' id='discover'>
+          <div className='flex flex-col items-start justify-start gap-y-2.5 text-black-100'>
+            <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+            <p>Explore out cars you might like</p>
+          </div>
+
+          <div className='mt-12 w-full flex-between items-center flex-wrap gap-5'>
+            <SearchBar />
+
+            <div className='home__filter-container'>
+              <CustomFilter /> 
+              {/* <CustomFilter title='fuel' options={fuels} />
+              <CustomFilter title='year' options={yearsOfProduction} /> */}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
